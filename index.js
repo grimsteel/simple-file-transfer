f8e98afadca313d7fc9e802da5d74ef3c0d4ee56
Siddhant <115331356+grimsteel@users.noreply.github.com>
2023-03-04 18:33:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { ExpressPeerServer } from "peer";
+const app = express();
+const server = app.listen(9000);
+
+const peerServer = ExpressPeerServer(server, {
+  path: '/',
+	proxied: true
+});
+
+app.use('/broker', peerServer);
+app.use(express.static("public"));
